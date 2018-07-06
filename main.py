a9a6c57584e589f9bf6133ddce27dcc1525fe747
Elliot Potts <elliot@skyzun.net>
2018-07-06 16:06:40 +0100

    Added main

---
+++ main.py
@@ -0,0 +1,15 @@
+
+"""
+Discord Bubble Text Generator
+    Written by Elliot Potts.
+    https://elliotpotts.me/
+
+
+Pretty sure something like this has already been achieved, I just thought I'd try and make it using Python.
+I might add a GUI later, but for now - this is a CLI.
+"""
+
+
+import time
+
+
